ece1142f0752146044f40bb7ddd3d731c0d16d5d
Rohit kumar Mishra <115352922+rhtmishra19@users.noreply.github.com>
2025-03-10 15:40:04 +0530

    Time: 644 ms (98.76%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -17,4 +17,4 @@
 -- FROM cte 
 -- WHERE email_validity = 'Valid';
 
-select * from Users where right(mail, 13) like "@leetcode.com" and left(mail, length(mail)-13) rlike '^[A-Za-z][A-Za-z0-9._-]*$'
+select * from Users where right(mail, 13) like "@leetcode.com" and left(mail, length(mail)-13) regexp '^[A-Za-z][A-Za-z0-9._-]*$'
