3827a1c8d254017b672e55eeb9bc2f6a812e58e8
Rohit kumar Mishra <115352922+rhtmishra19@users.noreply.github.com>
2025-03-10 15:47:23 +0530

    Time: 1236 ms (58.53%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -5,7 +5,7 @@
 --         mail, 
 --         SUBSTRING_INDEX(mail, '@', 1) AS before_at,
 --         CASE 
---             WHEN SUBSTRING_INDEX(mail, '@', 1) REGEXP '^[A-Za-z][A-Za-z0-9_.-]+$' 
+--             WHEN SUBSTRING_INDEX(mail, '@', 1) REGEXP '^[A-Za-z][A-Za-z0-9_.-]*$' 
 --             THEN 'Valid' 
 --             ELSE 'Invalid' 
 --         END AS email_validity
@@ -17,4 +17,14 @@
 -- FROM cte 
 -- WHERE email_validity = 'Valid';
 
-select * from Users where right(mail, 13) like "@leetcode.com" and left(mail, length(mail)-13) regexp '^[A-Za-z][A-Za-z0-9._-]*$'
+-- select * from Users where right(mail, 13) like "@leetcode.com" and left(mail, length(mail)-13) regexp '^[A-Za-z][A-Za-z0-9._-]*$'
+
+-- SELECT user_id, name, mail 
+-- FROM users 
+-- WHERE mail LIKE '%@leetcode.com'
+-- AND SUBSTRING_INDEX(mail, '@', 1) REGEXP '^[A-Za-z][A-Za-z0-9_.-]*$';
+
+SELECT user_id, name, mail
+FROM users
+WHERE 
+    mail REGEXP '^[A-Za-z][A-Za-z0-9._-]*@leetcode\\.com$';
